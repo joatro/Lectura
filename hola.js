4b269ffbfe9ce8fe9f2d4a704c26be8c54225e67
joatro <joatro@users.noreply.github.com>
2024-04-29 22:45:10 +0000

    ok

--- hola.js
+++ hola.js
@@ -1,12 +1,13 @@
 window.addEventListener('load',function(){
     
     var imagenes = [];
+    imagenes[0] = 'img/istapage.webp';
     imagenes[1] = 'img/istapage.webp';
   
    
 
     var indiceImagenes = 0;
-    var tiempo = 5000;
+    var tiempo = 4000;
     function cambiarImagenes(){
         
         document.slider.src = imagenes[indiceImagenes];
